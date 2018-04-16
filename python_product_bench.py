6f08ddb4835e4a3de12f8eefca7fe79ecad09507
davenza <daries100@gmail.com>
2018-04-16 11:33:06 +0200

    New results for i7 6700k.

--- python_product_bench.py
+++ python_product_bench.py
@@ -11,6 +11,6 @@ matrix2 = np.random.random_sample(tuple([1]*new_axes + [2]*size))
 '''
 
 for size in range(2, 13, 2):
-    time_taken = timeit.Timer('matrix1*matrix2', setup=setup.format(size)).repeat(10, 10000)
-    per_loop = min(time_taken) / 10000
+    time_taken = timeit.Timer('matrix1*matrix2', setup=setup.format(size)).repeat(10, 100000)
+    per_loop = min(time_taken) / 100000
     print('Benchmark ' + str(size) + ": " + str(per_loop*1000000) + " us")
